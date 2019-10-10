b3706977393b2a461b908a82801658e9e82a8117
Hossein Miraftabi <hossein.miraftabi@bjss.com>
2019-10-10 09:47:17 +0100

    add notification

--- my-vue/src/store/modules/event.js
+++ my-vue/src/store/modules/event.js
@@ -23,22 +23,40 @@ export const mutations = {
   }
 }
 export const actions = {
-  createEvent({ commit }, event) {
-    EventService.postEvent(event).then(() => {
-      commit('ADD_EVENT', event)
-    })
+  createEvent({ commit, dispatch }, event) {
+    EventService.postEvent(event)
+      .then(() => {
+        commit('ADD_EVENT', event)
+        const notification = {
+          type: 'success',
+          message: 'Your event has been created!'
+        }
+        dispatch('notification/add', notification, { root: true })
+      })
+      .catch(error => {
+        const notification = {
+          type: 'error',
+          message: 'There was a problem creating your event: ' + error.message
+        }
+        dispatch('notification/add', notification, { root: true })
+        throw error
+      })
   },
-  fetchEvents({ commit }, { perPage, page }) {
+  fetchEvents({ commit, dispatch }, { perPage, page }) {
     EventService.getEvents(perPage, page)
       .then(response => {
         commit('SET_TOTAL_EVENTS', response.headers['x-total-count'])
         commit('SET_EVENTS', response.data)
       })
       .catch(error => {
-        console.log('There was an error:' + error.response)
+        const notification = {
+          type: 'error',
+          message: 'There was a problem fetching events: ' + error.message
+        }
+        dispatch('notification/add', notification, { root: true })
       })
   },
-  fetchEventById({ commit, getters }, id) {
+  fetchEventById({ commit, getters, dispatch }, id) {
     const event = getters.getEventById(id)
 
     if (event) {
@@ -49,7 +67,11 @@ export const actions = {
           commit('SET_EVENT', response.data)
         })
         .catch(error => {
-          console.log(error)
+          const notification = {
+            type: 'error',
+            message: 'There was a problem fetching event: ' + error.message
+          }
+          dispatch('notification/add', notification, { root: true })
         })
     }
   }
