a92877e4df9943b9d48967bda06032bab4dccbbf
Hossein Miraftabi <hossein.miraftabi@bjss.com>
2019-10-16 21:56:47 +0100

    button tansition

---
+++ my-vue-animation/src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button @click="toggleModal">Open</button>
+
+    <transition name="fade">
+      <div v-if="isOpen">
+        <button @click="toggleModal">Close</button>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.isOpen = !this.isOpen;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-out;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
